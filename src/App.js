71b3be88489e35c2f9126314e9086f8e7f11fdbc
BIlnabil <nabilfiras93@gmail.com>
2022-06-25 06:54:40 +0700

    Tantangan: Pengenalan React

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Contact from "./components/Contact";
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from "./data/contacts.json";
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
+  const contactComponents = contacts.map((contact, idx) => {
+    return <Contact data={contact} key={idx} />;
+  });
   return (
     <div className="App">
-      
+      <Header />
+      {contactComponents}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,25 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
+import "./Contact.css";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = (props) => {
+  return (
+    <div className="contact-item">
+      <img
+        className="contact-bubble"
+        alt={props.data.name}
+        src={props.data.photo}
+      />
+      <div className="contact-container">
+        <div className="contact-info">
+          <h3 className="contact-name">{props.data.name}</h3>
+          <span className="contact-phone">{props.data.phone}</span>
+          <span className="contact-email">{props.data.email}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
